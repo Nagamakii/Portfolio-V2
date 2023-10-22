8af0699e45b4f7dfdab1b729c5a9f6524e33fab9
Evan Quah <you@example.com>
2023-10-22 13:14:12 -0500

    first commit

---
+++ portfolio-v2/app/page.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <head>
+        <title>Evan Quah Portfolio</title>
+        <meta name="description" content='Blank'></meta>
+        <link rel='icon' href='/favicon.ico' />
+      </head>
+
+      <main>
+        <section> className=" bg-white min-h-full"
+          <h1>Hello</h1>
+
+        </section>
+      </main>
+    </div>
+    )
+}
